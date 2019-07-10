907af57593e5ffb9793958bf042e28c20110eb3d
Kris Jordan <krisjordan@gmail.com>
2019-07-10 09:14:05 -0400

    Update motd

--- Dockerfile
+++ Dockerfile
@@ -3,6 +3,4 @@ FROM learncli/base:latest
 ADD setup.sh /
 RUN ["bash", "/setup.sh"]
 
-WORKDIR /mnt/workdir
-
 ENTRYPOINT ["bash"]
--- setup.sh
+++ setup.sh
@@ -3,8 +3,8 @@
 echo "PS1=\"learncli$ \"" >> /root/.bashrc
 
 echo -e "echo \"\n====================================================================\"" >> /root/.bashrc
-echo -e "echo \"\n  You are entering the learncli/bash Container \\\"Sandbox\\\" \n\"" >> /root/.bashrc
-echo -e "echo \"  To stop working in the container, use the command:\n\n    exit\"" >> /root/.bashrc
+echo -e "echo \"\n  You are now working inside a bash container. Welcome!\"" >> /root/.bashrc
+echo -e "echo \"  When you're done, you can quit with the command:\n\n    exit\"" >> /root/.bashrc
 echo -e "echo \"\n====================================================================\n\"" >> /root/.bashrc
 
 rm /setup.sh
